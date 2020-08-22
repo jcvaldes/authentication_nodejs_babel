20dfc56dad48b3d291538fc8ca9afbcbda36c86b
Juan Carlos Valdés <idevkingos@gmail.com>
2020-08-22 19:16:25 -0300

    arquitectura base

--- seeders/20200820234928-users.js
+++ seeders/20200820234928-users.js
@@ -3,9 +3,9 @@ const bcrypt = require('bcryptjs');
 /* eslint-disable no-unused-vars */
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Users', [{
-      firstname: 'Lautaro',
-      lastname: 'Rivero',
-      email: 'lautaro@gmail.com',
+      firstname: 'Tony',
+      lastname: 'Stark',
+      email: 'tony@gmail.com',
       password: bcrypt.hashSync('123456', 10),
       active: true,
       createdAt: new Date(),
